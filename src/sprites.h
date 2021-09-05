e6f5731e0075802f133c6cd80fa75e99f9eec06b
Wendel Scardua <wendelscardua@gmail.com>
2021-09-05 20:48:25 -0300

    Tweak sprite coordinates

--- src/sprites.h
+++ src/sprites.h
@@ -1,15 +1,15 @@
 #include "constants.h"
 const unsigned char PiranhaSpriteR[]={
-                                      - 3,- 4,0x00,2,
+                                      - 7,- 7,0x00,2,
                                       128
 };
 
 const unsigned char PiranhaSpriteL[]={
-                                      - 4,- 4,0x00,2|OAM_FLIP_H,
+                                      - 7,- 7,0x00,2|OAM_FLIP_H,
                                       128
 };
 
 const unsigned char DeadPiranhaSprite[]={
-                                         - 3,- 3,0x01,2,
+                                         - 7,- 7,0x01,2,
                                          128
 };
